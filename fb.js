f0a52b2a5f04f5e52c31520c4ccd64a111389e75
Mujtaba Asif <mjtbasif@gmail.com>
2020-06-13 05:23:01 +0600

    this script is the real deal.
    customize it to get hide or change more stuffs :3

---
+++ fb.js
@@ -0,0 +1,2 @@
+document.getElementById("pagelet_gaming_destination_rhc").style.display = "none";
+document.getElementById("u_0_c").getElementsByTagName("A")[0].setAttribute("href","http://www.facebook.com/");
